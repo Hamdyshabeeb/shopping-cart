1debd3532155e703bf7eeaba7f5a775191ad8b2a
Hamdy Shabeeb <34157559+Hamdyshabeeb@users.noreply.github.com>
2024-05-25 13:40:01 +0300

    create home view use react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Navbar from './components/Navbar';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+
 function App() {
-	return <div>App</div>;
+	return (
+		<>
+			<Navbar />
+
+			<Routes>
+				<Route index element={<Home />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
---
+++ src/views/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+	return (
+		<section>
+			<h2>Welcome To the Store </h2>
+		</section>
+	);
+}
+
+export default Home;
