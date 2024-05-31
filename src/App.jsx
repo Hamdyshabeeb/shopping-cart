34e75d9b12c89cb6d007f86e53c4e0cee40e8993
Hamdy Shabeeb <34157559+Hamdyshabeeb@users.noreply.github.com>
2024-06-01 00:24:30 +0300

    success view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Cancel from './views/Cancel';
+import Success from './views/Success';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Routes>
 				<Route index element={<Home />} />
 				<Route path="/cancel" element={<Cancel />} />
+				<Route path="/success" element={<Success />} />
 			</Routes>
 		</>
 	);
---
+++ src/views/Success.jsx
@@ -0,0 +1,12 @@
+function Success() {
+	return (
+		<div className="wrapper flex min-h-[90vh] justify-center items-center">
+			<h2 className="text-gray-700 text-4xl -mt-[80px]">
+				{' '}
+				Thank you for your purchase!{' '}
+			</h2>
+		</div>
+	);
+}
+
+export default Success;
